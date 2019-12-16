6b79e2e14ac2c6916400d2cef758f829a8a34711
mihakoyamatsu <miharipannda@gmail.com>
2019-12-16 04:48:19 +0000

    order newページ、application.jsページ編集

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,14 @@
 
 //= require_tree .
 
+$(function() {
+  $('[name="btn"]:radio').change( function() {
+    if($('[id=a]').prop('checked')){
+      $('.text').fadeOut();
+      $('.text01').fadeIn();
+    } else if ($('[id=b]').prop('checked')) {
+      $('.text').fadeOut();
+      $('.text02').fadeIn();
+    } 
+  });
+});
