face71d7f8481f1a885d06785379a535403ef074
mihakoyamatsu <miharipannda@gmail.com>
2019-12-17 04:26:14 +0000

    購入確認ページ編集

---
+++ app/assets/javascripts/order_new.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('[name="btn"]:radio').change( function() {
+    if($('[id=a]').prop('checked')){
+      $('.text').fadeOut();
+      $('.text01').fadeIn();
+    } else if ($('[id=b]').prop('checked')) {
+      $('.text').fadeOut();
+      $('.text02').fadeIn();
+    }
+  });
+});
+
+$(function(){
+    var nowchecked = $('input[name=btn]:checked').val();
+    $('input[name=btn]').click(function(){
+        if($(this).val() == nowchecked) {
+            $(this).prop('checked', false);
+            nowchecked = false;
+        } else  {
+            nowchecked = $(this).val();
+        }
+    });
+});
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,10 @@ class Order < ApplicationRecord
 	validates :delivery_name, presence: true
 	validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
 	validates :destination, presence: true
+
+    enum payment:{
+    クレジットカード: 0,
+    銀行振込: 1,
+    代引き: 2
+  }
 end
