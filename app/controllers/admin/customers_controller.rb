7ecc1587a840e6366b9bfa491efeb2c484005331
matsu-5 <tanagosakana@gmail.com>
2019-12-17 08:54:10 +0000

    [ADD]顧客一覧ページ[update]admin/customersコントローラー

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -7,13 +7,26 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+  	@customer = Customer.find(params[:id])
   end
 
   def index
-  	@customer = current_user
     @customers =Customer.all
   end
 
   def update
+  	@customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
   end
 end
+private 
+def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:phone_number,:email)
+end
+def correct_user
+    user = Customer.find(params[:id])
+    if current_user != customer
+      redirect_to admin_customer_path(current_user)
+    end
+  end
