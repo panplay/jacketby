da94a5f883126d672b0f92a57398447ca9b94bb5
mihakoyamatsu <miharipannda@gmail.com>
2019-12-17 08:42:07 +0000

    [add]cart controller,cart showページ

--- app/controllers/customers/carts_controller.rb
+++ app/controllers/customers/carts_controller.rb
@@ -1,20 +1,27 @@
 class Customers::CartsController < ApplicationController
-  
+  #before_action :setup_cart!, only: [:create, :update, :destroy]
   def create
-  	  @customer = current_customer
-      @item = Item.find(params[:item_id])
-      newcart = Cart.new(cart_params)
-      newcart.customer_id = current_customer.id
-      newcart.item_id = @item.id
-      newcart.save
-      redirect_to new_customer_orders_path(newcart.id)
+      #@customer = current_customer
+  	  @item = Item.find(params[:item_id])
+      @newcart = Cart.new(cart_params)
+      @newcart.customer_id = current_customer.id
+      @newcart.item_id = @item.id
+      @newcart.save
+      redirect_to customers_item_path(@item)
+      #if
+      #@newcart.blank?
+      #@newcart = current_customer.cart.build(item_id: params[:item_id])
+      #end
+      #@newcart.quantity += params[:quantity].to_i
+      #@newcart.save
   end
 
   def show
-      newcart = Cart.new
-      carts = Cart.all
-      # @item = Item.find(params[:id])
-      @customer = current_customer
+      #@newcarts = current_customer.carts
+      @carts = Cart.all
+      @item = Item.find(params[:id])
+      @cart = Cart.find(params[:id])
+      #@customer = current_customer
   end
 
   def update
@@ -27,6 +34,10 @@ class Customers::CartsController < ApplicationController
   def cart_params
       params.require(:cart).permit(:item_id, :customer_id, :quantity)
   end
+
+  #def setup_cart_item!
+    #@newcart = current_customer.cart.find_by(item_id: params[:item_id])
+  #end
 
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '75c907c2bd96cea9778e706b6f76c0ce6bc04f96f5da1f1328c635b454584ce928300068494562f628b90da5892cdc1d14c4fdeadfa0cd99d16724781a137cc4'
