831c0e5afc7fa658655350104072980235df869f
mihakoyamatsu <miharipannda@gmail.com>
2019-12-18 04:09:36 +0000

    [edit]カート詳細show destroy

--- app/controllers/customers/carts_controller.rb
+++ app/controllers/customers/carts_controller.rb
@@ -1,5 +1,4 @@
 class Customers::CartsController < ApplicationController
-  before_action :setup_cart!, only: [:create, :update, :destroy]
   def create
   	  item = Item.find(params[:item_id])
       cart = current_customer.carts.new(cart_params)
@@ -13,23 +12,22 @@ class Customers::CartsController < ApplicationController
   end
 
   def update
+    item = Item.find(params[:item_id])
+    cart = current_customer.carts.find_by(item_id: item.id)
+    cart.update
+    redirect_to customers_path(item)
+
   end
 
   def destroy
     item = Item.find(params[:item_id])
     cart = current_customer.carts.find_by(item_id: item.id)
     cart.destroy
-    reLdirect_to customers_item_path(item)
+    redirect_to customers_path(item)
   end
 
   private
-  #def cart_params
-      #params.require(:cart).permit(:item_id, :customer_id, :quantity)
-  #end
-
-  def setup_cart
-    @cart_item = current_cart.cart.find_by(item_id: params[:item_id])
-  #end
-
-
+  def cart_params
+      params.require(:cart).permit(:item_id, :customer_id, :quantity)
+  end
 end
