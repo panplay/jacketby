d20358c31bb43a27bcc4ad900118b54ee0879f84
kurahashitakahiro <takaxmaki@gmail.com>
2019-12-20 09:21:54 +0000

    [update]arrival_item

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,8 @@ Rails.application.routes.draw do
     put 'artists/hide'
 
     get 'homes/index' => 'homes#index'
+    get "search" => "arrival_items#search"
+    get "history_search" => "arrival_items#history_search"
 
    put "/items/:id" => "items#hide"
    resources :items, only: [:new, :create, :edit, :update, :index]
@@ -56,7 +58,6 @@ Rails.application.routes.draw do
     end
 
     put "/customers/:id" => "customers#hide"
-    get "/customers/:id" => "customers#favorites", as: 'favorites'
     resources :customers, only: [:edit,:update]
     put "/customers/:id" => "customers#hide"
     get "/customers/:id" => "customers#favorites", as: 'favorites'
