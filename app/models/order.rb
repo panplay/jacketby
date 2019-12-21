64c1783948965cd31241c81fdf1255f72eecc78c
yuto1014 <yto.oct.14@gmail.com>
2019-12-21 07:02:49 +0000

    [Update]受注一覧

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,13 +7,13 @@ class Order < ApplicationRecord
 	validates :delivery_name, presence: true
 	validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
 	validates :destination, presence: true
-	
+
     enum payment:{
     クレジットカード: 0,
     銀行振込: 1,
     代引き: 2
   }
-    
+
     enum status:{
     受付: 0,
     商品準備中: 1,
